8f93882e491b0eb7d9db3e49d9ed57086264a92a
slauayama <simonlauyamauchi@gmail.com>
2014-06-11 16:19:50 -0700

    Working on implementing templating Created a new branch just in case it doesnt work Also am working on creating a top list panel view

--- js/views/racksView.js
+++ js/views/racksView.js
@@ -1,36 +1,36 @@
-define(['views/rackView'], 
-	function(RackView){
-		var RacksView = Backbone.Marionette.CollectionView.extend({
-		    tagName: 'scene',
-		    itemViewContainer: '#x3dScene',
+define([
+	'views/rackView'
+], function(RackView){
+	var RacksView = Backbone.Marionette.CollectionView.extend({
+	    tagName: 'scene',
+	    itemViewContainer: '#x3dScene',
 
-		    attributes: function() {
-		    	// Specifically added attributes to ensure loading on
-		        return {
-		            render: true,
-		        };
-		    },
+	    attributes: function() {
+	    	// Specifically added attributes to ensure loading on
+	        return {
+	            render: true,
+	        };
+	    },
 
-		    //itemview is marionette
-		    itemView: RackView,
+	    //itemview is marionette
+	    itemView: RackView,
 
-		    initialize: function() {
+	    initialize: function() {
 
-		    },
+	    },
 
-		    // Becuase I am using Marionette
-		    // Whenever I pass in a collection to the RackView
-		    // It will automiatically render
-		    // render: function() {
-		    onBeforeRender: function() {
-		        
-		    },
-		    onRender: function() {
-		           
-		    }
+	    // Becuase I am using Marionette
+	    // Whenever I pass in a collection to the RackView
+	    // It will automiatically render
+	    // render: function() {
+	    onBeforeRender: function() {
+	        
+	    },
+	    onRender: function() {
+	           
+	    }
 
-		});
-		console.log("returning Rack(s)View");
-		return RacksView;
-	}
-);
+	});
+
+	return RacksView;
+});
