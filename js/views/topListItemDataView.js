a9657aeb44bd030d098027b6bcae143d0e7daa62
Simon <simonlauyamauchi@gmail.com>
2014-07-21 09:24:41 -0700

    Using onShow instead of onRender. This will allow me to access objects that have been rendered but haven't been appended to the page.

--- js/views/topListItemDataView.js
+++ js/views/topListItemDataView.js
@@ -6,33 +6,24 @@ define([
 	var TopListItemDataView = Backbone.Marionette.ItemView.extend({
 		tagName: 'li',
 		template: _.template(TopListItemDataTemplate),
-		templateHelpers: {
-		},
 
-		onRender: function() {
-			// Cannot figure out for all my life how to access the width properties
-			// With the width properties I could detect if there is overflow or not
-			// Then I will use qtip to make a tool tip
-
-			// Problem is that item is not fully rendered. Is rendered but is not on page
-			_.defer(function() {
-                // console.log(this, this.el.clientWidth, this.el.scrollWidth);
-                if (this.el.scrollWidth > this.el.clientWidth){
-                	this.$el.qtip({
-                		content: {
-				        	text: this.model.get('racks')
-    					},
-    					style: {
-    						width: 175
-    					},
-    					position: {
-                            my: 'bottom left',
-                            at: 'right top'
-                        }
-    				});
-                }
-            }.bind(this));
-			
+		// Using onShow instead of onRender. 
+		// This will run the code when it is displayed to the page.
+		onShow: function() {
+            if (this.el.scrollWidth > this.el.clientWidth){
+            	this.$el.qtip({
+            		content: {
+			        	text: this.model.get('racks')
+					},
+					style: {
+						width: 175
+					},
+					position: {
+                        my: 'bottom left',
+                        at: 'right top'
+                    }
+				});
+            } 
 		}
 	});
 
